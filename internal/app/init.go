313eb75cddd0585dd438243171643059951e87f7
Ari Firdaus <af.arifirdaus@gmail.com>
2021-11-07 17:44:53 +0700

    add handler for methodnotallowed and notfound

--- internal/app/init.go
+++ internal/app/init.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"deuvox/pkg/handler"
+
 	"github.com/go-chi/chi"
 )
 
@@ -12,6 +14,8 @@ func New() App {
 	r := chi.NewRouter()
 	d := initDelivery()
 	r.Post("/auth/login", d.auth.Login)
+	r.NotFound(handler.NotFound)
+	r.MethodNotAllowed(handler.MethodNotAllowed)
 	return App{
 		R: r,
 	}
